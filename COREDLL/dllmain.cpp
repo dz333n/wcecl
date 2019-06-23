f9cc88d697adbae743e9ff4251bd3cee1d75337f
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-06-23 22:33:10 +0300

    Add messagebox on dll_attach

--- COREDLL/dllmain.cpp
+++ COREDLL/dllmain.cpp
@@ -1,20 +1,24 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-		// MessageBox(NULL, L"WinCeCompatLayer CoreDLL Attached", L"WinCeCompatLayer - Coredll", 0);
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		MessageBox(NULL, L"Library attached!", L"Windows CE Compatibility", 0);
+		break;
+	};
+	//
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
