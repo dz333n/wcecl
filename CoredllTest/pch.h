b1976a0638d44a565628e9918f5740fdbc824018
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-07-07 23:51:12 +0300

    Fix x86 build ._.

--- CoredllTest/pch.h
+++ CoredllTest/pch.h
@@ -1,6 +1,7 @@
 #ifndef PCH_H
 #define PCH_H
 
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 
 int __cdecl RegisterDefaultGestureHandler();
