11f4563cade19dc98d7f91fe87a1af1122a4a5cf
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-07-31 22:43:31 +0300

    Probably fix sndPlaySoundW

--- COREDLL/mmsystem_wcecl.cpp
+++ COREDLL/mmsystem_wcecl.cpp
@@ -2,12 +2,33 @@
 #include "stdafx.h"
 #include <MMSystem.h>
 
+#ifdef __stdcall
+#undef __stdcall
+typedef BOOL(__stdcall *sndPlaySoundW_Invoke)(LPCWSTR lpszSoundName, UINT fuSound);
+#define __stdcall __cdecl
+#endif
+
 // Functions
 BOOL APIENTRY sndPlaySoundW_WCECL(LPCWSTR lpszSoundName, UINT fuSound)
 {
-	return FALSE;
-	// auto result = sndPlaySoundW(lpszSoundName, fuSound); // unresolved for some reason, please fix
-	// return result;
+	HINSTANCE h = LoadLibraryW(L"winmm.dll");
+
+	if (w32err(!h))
+	{
+		auto win32error = GetLastError();
+		DebugBreak();
+	}
+
+	sndPlaySoundW_Invoke func = (sndPlaySoundW_Invoke)GetProcAddress(h, "sndPlaySoundW");
+
+	if (w32err(!func))
+		DebugBreak();
+
+	auto result = func(lpszSoundName, fuSound);
+
+	FreeLibrary(h);
+
+	return result;
 }
 
 // Stubs
