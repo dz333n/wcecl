ebbab05d12d2a2756ae606d7b31e6dd312ccdae8
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-08-03 23:44:55 +0300

    Update winuser_wcecl.h

--- COREDLL/winuser_wcecl.h
+++ COREDLL/winuser_wcecl.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Window Style
 #define WINCE_WS_OVERLAPPED       WINCE_WS_BORDER | WINCE_WS_CAPTION
 #define WINCE_WS_CLIPSIBLINGS     0x04000000L
 #define WINCE_WS_CLIPCHILDREN     0x02000000L
@@ -15,6 +16,7 @@
 #define WINCE_WS_SIZEBOX          WINCE_WS_THICKFRAME
 #define WINCE_WS_POPUP            0x80000000L
 
+// Window Style Extended
 #define WINCE_WS_EX_DLGMODALFRAME     0x00000001L
 #define WINCE_WS_EX_TOPMOST           0x00000008L
 #define WINCE_WS_EX_TOOLWINDOW        0x00000080L
@@ -34,7 +36,7 @@
 #define WINCE_WS_EX_INK				  0x10000000L
 #define WINCE_WS_EX_NOANIMATION		  0x04000000L
 
-
+// Show Window
 #define WINCE_SW_HIDE               0
 #define WINCE_SW_SHOWNORMAL         1
 #define WINCE_SW_SHOWNOACTIVATE     4
@@ -45,6 +47,7 @@
 #define WINCE_SW_MAXIMIZE			12
 #define WINCE_SW_RESTORE			13
 
+// Class Style
 #define WINCE_CS_VREDRAW          0x0001
 #define WINCE_CS_HREDRAW          0x0002
 #define WINCE_CS_DBLCLKS          0x0008
@@ -53,3 +56,13 @@
 #define WINCE_CS_SAVEBITS         0x0800
 #define WINCE_CS_GLOBALCLASS      0x4000
 #define WINCE_CS_IME              0x00010000
+
+// Dialog Style
+#define WINCE_DS_LOCALEDIT        0x20L   /* Edit items get Local storage. */
+#define WINCE_DS_SETFONT          0x40L   /* User specified font for Dlg controls */
+#define WINCE_DS_MODALFRAME       0x80L   /* Can be combined with WS_CAPTION  */
+#define WINCE_DS_SETFOREGROUND    0x200L  /* not in Windows 3.1 */
+#define WINCE_DS_3DLOOK           0x0004L
+#define WINCE_DS_CONTROL          0x0400L
+#define WINCE_DS_CENTER           0x0800L
+#define WINCE_DS_NOCLOSEBTN		0x4000L
