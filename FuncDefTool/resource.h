feb2710767c75d6657b2d1e79afa694aa891c0ed
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-08-06 12:57:51 +0300

    [FuncDefTool] More GUI controls + fix ARM

--- FuncDefTool/resource.h
+++ FuncDefTool/resource.h
@@ -3,6 +3,8 @@
 // Used by FuncDefTool.rc
 //
 #define IDC_MYICON                      2
+#define IDOK2                           2
+#define ID_COPY                         2
 #define IDD_FUNCDEFTOOL_DIALOG          102
 #define IDS_APP_TITLE                   103
 #define IDM_ABOUT                       104
@@ -12,8 +14,13 @@
 #define IDC_FUNCDEFTOOL                 109
 #define IDR_MAINFRAME                   128
 #define IDD_MAINDLG                     130
-#define IDCANCEL                        1000
+#define IDC_SELECT_WCECL                1001
+#define IDC_SELECT_TARGET               1002
+#define IDC_TARGET_PATH                 1003
+#define IDC_CB_ADD_COMMENTS             1005
+#define IDC_RESULT_BOX                  1006
 #define IDC_STATIC                      -1
+#define IDC_WCECL_PATH                  -1
 
 // Next default values for new objects
 // 
@@ -22,7 +29,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
