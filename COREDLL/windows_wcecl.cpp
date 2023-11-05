999919582dd04d332a0ce0c41596662deecef499
Yaroslav Kibysh <yanet.prod@gmail.com>
2023-11-05 22:38:21 +0200

    Add EnumFontFamiliesW_WCECL

--- COREDLL/windows_wcecl.cpp
+++ COREDLL/windows_wcecl.cpp
@@ -1,4 +1,14 @@
 // windows_wcecl.cpp : wce/windows.h functions
 #include "stdafx.h"
 
+int EnumFontFamiliesW_WCECL(
+	HDC           hdc,
+	LPCWSTR       lpLogfont,
+	FONTENUMPROCW lpProc,
+	LPARAM        lParam)
+{
+	auto result = ::EnumFontFamilies(hdc, lpLogfont, lpProc, lParam);
+	return result;
+}
+
 Stub(GetMouseMovePoints_WCECL);
