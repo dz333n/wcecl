e654a855696b81870b7c734e3da30690cb92dd03
Marcin Jabłoński <nnxcomputing@gmail.com>
2024-04-14 00:56:40 +0200

    Remove unnecessary __stdcall defines

--- COREDLL/dllmain.cpp
+++ COREDLL/dllmain.cpp
@@ -1,7 +1,6 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 
-#undef __stdcall // DllMain should be __stdcall always
 BOOL __stdcall DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
